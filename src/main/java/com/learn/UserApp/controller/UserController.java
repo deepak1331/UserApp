639c07c5c1291b079f3a0ac575864e0622d9dc9b
Deepak Yadav <deepakyadav1331@gmail.com>
2025-08-06 14:15:44 +0530

    Implemented @CircuitBreaker and fallback methods

--- src/main/java/com/learn/UserApp/controller/UserController.java
+++ src/main/java/com/learn/UserApp/controller/UserController.java
@@ -2,13 +2,17 @@ package com.learn.UserApp.controller;
 
 import com.learn.UserApp.entity.User;
 import com.learn.UserApp.service.UserService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import lombok.extern.log4j.Log4j2;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@Log4j2
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -22,12 +26,25 @@ public class UserController {
     }
 
     @RequestMapping(path = "/{userId}", method = RequestMethod.GET)
-    public ResponseEntity<User> getUser(@PathVariable String userId){
+    @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
+    public ResponseEntity<User> getUser(@PathVariable String userId) {
         return ResponseEntity.ok(service.getUserById(userId));
     }
 
+    public ResponseEntity<User> ratingHotelFallback(String userId, Exception exception) {
+        log.info("User - > Rating Fallback is called because service is down: {}",
+                exception.getMessage());
+        User user = User.builder().username("Dummy User")
+                .id("0000-0000-0000-0000")
+                .about("This is a dummy response, since rating service is down")
+                .email("dummy@gmail.com").build();
+        return ResponseEntity.ok(user);
+
+    }
+
+
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<User>> getUsers(){
+    public ResponseEntity<List<User>> getUsers() {
         return ResponseEntity.ok(service.getAllUsers());
     }
 }
--- src/main/java/com/learn/UserApp/service/impl/UserServiceImpl.java
+++ src/main/java/com/learn/UserApp/service/impl/UserServiceImpl.java
@@ -9,18 +9,17 @@ import com.learn.UserApp.repo.UserRespository;
 import com.learn.UserApp.service.UserService;
 import com.learn.UserApp.service.external.HotelService;
 import com.learn.UserApp.service.external.RatingService;
+import lombok.extern.java.Log;
 import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 import java.util.stream.Collectors;
 
-import static com.learn.UserApp.constant.AppConstants.NOT_FOUND;
-import static com.learn.UserApp.constant.AppConstants.RATING_BY_USER;
+import static com.learn.UserApp.constant.AppConstants.*;
 
 @Log4j2
 @Service
@@ -52,7 +51,8 @@ public class UserServiceImpl implements UserService {
     @Override
     public List<User> getAllUsers() {
         log.info("Fetching All Users");
-        return repository.findAll();
+        List<User> userList = repository.findAll();
+        return userList;
     }
 
     @Override
@@ -64,35 +64,16 @@ public class UserServiceImpl implements UserService {
         if (user != null) {
             log.info("Calling Rating Service: {}", RATING_BY_USER + user.getId());
 
-            //Using RestTemplate
-            //Rating[] ratings = restTemplate.getForObject(RATING_BY_USER + user.getId(), Rating[].class);
-            //log.info("Rating Service Response Size: {}", ratings != null ? ratings.length : 0);
-            //List<Rating> ratingList = Arrays.stream(ratings).toList();
-
-
             //Using Feign Client
             List<Rating> ratingList = ratingService.getRatingByUserId(user.getId());
-            //List<RatingDto> ratingDtoList = new ArrayList<>();
-                    //ratingList.stream().map(rating -> mapper.map(rating, RatingDto.class)).collect(Collectors.toList());
             if (!ratingList.isEmpty()) {
-                ratingList.stream().map(
+                ratingList = ratingList.stream().map(
                         rating -> {
-                            //Hotel hotel = restTemplate.getForObject(HOTEL_SERVICE + rating.getHotelId(), Hotel.class);
                             Hotel hotel = hotelService.getHotelById(rating.getHotelId());
-                            //rating.setHotel(hotel);
                             rating.setHotel(mapper.map(hotel, HotelDto.class));
-
-                            //Using Mapper to convert Entity into DTO class
-                            /*TypeMap<Rating, RatingDto> propertyMapper = this.mapper.createTypeMap(Rating.class, RatingDto.class);
-                            propertyMapper.addMappings(mapper -> mapper.skip(RatingDto::setHotel));
-                            RatingDto ratingDto  = propertyMapper.map(rating);
-                            ratingDto.setHotel(mapper.map(hotel, HotelDto.class));
-                            ratingDtoList.add(ratingDto);*/
-
                             return rating;
-                        }).collect(Collectors.toList());
+                        }).toList();
             }
-            //user.setRatings(ratingDtoList);
             user.setRatings(ratingList);
         }
         return user;
