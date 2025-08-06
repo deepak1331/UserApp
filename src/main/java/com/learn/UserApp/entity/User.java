7c49ee7334220458ef415667297bc2197c6b6018
Deepak Yadav <deepakyadav1331@gmail.com>
2025-08-06 14:13:52 +0530

    Added lambok @Builder annotation

--- src/main/java/com/learn/UserApp/entity/User.java
+++ src/main/java/com/learn/UserApp/entity/User.java
@@ -4,6 +4,7 @@ import com.learn.UserApp.model.Rating;
 import com.learn.UserApp.model.dto.RatingDto;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
@@ -16,6 +17,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Builder
 @Table(name = "micro_user")
 public class User {
 
