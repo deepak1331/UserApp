5b3878ca8198e89b9063be7b47c77091b822fe13
Deepak Yadav <deepakyadav1331@gmail.com>
2025-08-07 15:18:57 +0530

    Implemented @RateLimiter usage

--- src/main/java/com/learn/UserApp/controller/UserController.java
+++ src/main/java/com/learn/UserApp/controller/UserController.java
@@ -3,6 +3,7 @@ package com.learn.UserApp.controller;
 import com.learn.UserApp.entity.User;
 import com.learn.UserApp.service.UserService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,8 +45,11 @@ public class UserController {
     }
 
     //@CircuitBreaker Usage
+    //@CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
+
+    //@RateLimiter usage
+    @RateLimiter(name = "userRateLimiter", fallbackMethod = "ratingHotelFallback")
     @RequestMapping(path = "/{userId}", method = RequestMethod.GET)
-    @CircuitBreaker(name = "ratingHotelBreaker", fallbackMethod = "ratingHotelFallback")
     public ResponseEntity<User> getUser(@PathVariable String userId) {
         return ResponseEntity.ok(service.getUserById(userId));
     }
